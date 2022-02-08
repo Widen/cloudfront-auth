acc18efda8e232f45a6fe502a7f5da1b2912441e
Mark Skelton <mdskelton99@gmail.com>
2022-02-08 15:58:44 -0600

    Simplify manifests (#98)
    * Move to a single manifest
    
    * Simplify build script
    
    * Format ISC license

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ ! -d "distributions" ]; then
-  if [ ! -L "distributions" ]; then
-    mkdir distributions
-  fi
-fi
-
-npm run-script build
+mkdir -p distributions
+npm run build
